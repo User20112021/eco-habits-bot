02ece72213cd1524b540463a26517928d99a3951
User20112021 <94719711+User20112021@users.noreply.github.com>
2026-02-11 11:39:45 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,7 +30,7 @@ ADMIN_ID = 7919965678
 admin_delete_pending = False
 TIMEZONE = os.getenv("BOT_TZ", "Europe/Istanbul")
 PING_HOUR = int(os.getenv("BOT_PING_HOUR", "11"))
-PING_MINUTE = int(os.getenv("BOT_PING_MINUTE", "30"))
+PING_MINUTE = int(os.getenv("BOT_PING_MINUTE", "48"))
 DB_PATH = os.getenv("BOT_DB_PATH", "eco_tracker.db")
 
 # Доступные классы в первом релизе
